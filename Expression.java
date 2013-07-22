2634f498ae3bc6ad85b1d8a3466c771a30355a2d
Peter Polinsky <ppolinsky@gmail.com>
2013-07-22 08:10:26 -0700

    more line parsing

--- Expression.java
+++ Expression.java
@@ -1,75 +1,193 @@
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-
 public class Expression {
-	private String reason;
-	private String express;
+	private ExpNode myRoot;	
+	
+	// not sure if this will be necessary
+	public Expression ( ) {
+		myRoot = null;
+	}
+	
+	public Expression (ExpNode t) {
+		myRoot = t;
+	}
 	
 	public Expression (String s) throws IllegalLineException {
-		expressionParser(s);
+		myRoot = expTreeHelper(s);
+	}	
+
+	private ExpNode expTreeHelper (String expr) throws IllegalLineException {
+	    if (expr.charAt (0) != '(') {
+	    	// if not parenthesized, the only possible legal inputs are a or ~a
+	    	if (expr.length() == 1 && Character.isLetter(expr.charAt(0))) {
+	    		return new ExpNode(expr.charAt(0));
+	    	} else if (expr.length() == 2 && expr.charAt(0) == '~' && Character.isLetter(expr.charAt(1))) {
+	    		return new ExpNode(expr.charAt(0), null, new ExpNode(expr.charAt(1)));
+	    	} else {
+	    		throw new IllegalLineException("bad line");
+	    	}
+	    } else {
+	        // expr is a parenthesized expression.
+	        // Strip off the beginning and ending parentheses,
+	        // find the main operator (an occurrence of &, |, or => 
+	    	// not nested in parentheses), and construct the two subtrees.
+	        int nesting = 0;
+	        int opPos = 0;
+	        boolean isFollows = false;
+	        for (int k=1; k<expr.length()-1; k++) {
+	            if (expr.charAt(k) == '(') { 
+	            	nesting++;
+	            } else if (expr.charAt(k) == ')') {
+	            	nesting--;
+	            } else if (nesting == 0 && (expr.charAt(k) == '&' || expr.charAt(k) == '|')) {
+	            	opPos = k;
+	            	break;
+	            } else if (nesting == 0 && (expr.charAt(k) == '=' 
+	            		&& expr.length() > k+1 && expr.charAt(k+1) == '>')) {
+	            	opPos = k;
+	            	isFollows = true;
+	            	break;
+	            }
+	        }
+	        String opnd1 = expr.substring (1, opPos);
+	        String opnd2, op;
+	        if (isFollows) {
+	        	opnd2 = expr.substring (opPos+2, expr.length()-1);
+		        op = expr.substring (opPos, opPos+2);	        	
+	        } else {
+		        opnd2 = expr.substring (opPos+1, expr.length()-1);
+		        op = expr.substring (opPos, opPos+1);	        	
+	        }
+	        return new ExpNode (op, expTreeHelper(opnd2), expTreeHelper(opnd1)); 
+	    }
+	}	
+	
+	
+	// Print the values in the tree in preorder: root value first,
+	// then values in the left subtree (in preorder), then values
+	// in the right subtree (in preorder).
+	public void printPreorder ( ) {
+		printPreorder (myRoot);
+		System.out.println ( );
+	}
+
+	private static void printPreorder (ExpNode t) {
+		if (t != null) {
+			System.out.print (t.myItem + " ");
+			printPreorder (t.myLeft);
+			printPreorder (t.myRight);
+		}
+	}
+
+	// Print the values in the tree in inorder: values in the left
+	// subtree first (in inorder), then the root value, then values
+	// in the right subtree (in inorder).
+	public void printInorder ( ) {
+		printInorder (myRoot);
+		System.out.println ( );
+	}
+
+	private static void printInorder (ExpNode t) {
+		if (t != null) {
+			printInorder (t.myLeft);
+			System.out.print (t.myItem + " ");
+			printInorder (t.myRight);
+		}
+	}
+	
+//	public void fillSampleTree1 ( ) {
+//		myRoot =
+//		    new TreeNode ("a",
+//			new TreeNode ("b"),
+//			new TreeNode ("c"));
+//	}
+	
+	public static int height(ExpNode n) {
+		if (n == null) {
+			return 0;
+		} else if (n.isLeaf()) {
+			return 1;
+        } else {
+            return Math.max(height(n.myLeft), height(n.myRight)) + 1;
+        }
+	}
+	
+	public boolean isCompletelyBalanced() {
+		if (myRoot == null) {
+			return true;
+		} else {
+			return isCompletelyBalancedHelper(myRoot);
+		}
+	}
+	
+	private boolean isCompletelyBalancedHelper(ExpNode n) {
+		if (n.myLeft == null && n.myRight == null) {
+			return true;
+		} else if ((n.myLeft == null && n.myRight != null) ||
+					(n.myLeft != null && n.myRight == null)) {
+			return false;
+		} else {
+			return (isCompletelyBalancedHelper(n.myLeft) 
+					&& isCompletelyBalancedHelper(n.myRight));
+		}
+		
+	}
+	
+	public void print ( ) {
+	    if (myRoot != null) {
+	        printHelper (myRoot, 0);
+	    }
 	}
 		
-
-	public class expressionNode {
-		private Object operator = null;
-		private expressionNode variables = null;
-
-			public expressionNode(Object operatorObj, expressionNode variables) {
-					
-			}
-
-			public void addExpression(Object operatorObj, String data) {
-					if (operator != null) {
-						expressionNode.addExpressionHelper(operatorObj, data);
-					}
-			}
-
-			public static void addExpressionHelper(Object operatorObj, String data) {
-				if () {
-				
-				} else {
-					Iterator<expressionNode> iter = expressionNode.variables.iterator();
-					while (iter.hasNext()) {
-						expressionNode.addExpressionHelper( iter.next());}
-				}
-			}
+	private static final String indent1 = "    ";
+		
+	private static void printHelper (ExpNode root, int indent) {
+	    if (root.myLeft != null) printHelper(root.myLeft, indent+1);
+	    println (root.myItem, indent);
+	    if (root.myRight != null) printHelper (root.myRight, indent+1) ;
 	}
-				
+			
+	private static void println (Object obj, int indent) {
+	    for (int k=0; k<indent; k++) {
+	        System.out.print (indent1);
+	    }
+	    System.out.println (obj);
+	}
+	
 
-			public void expressionParser (String x) {
-				if(x.contains("print")){
-					reason = "print";
-				}else {
-					if(x.contains("show")){
-						reason = "show";
-						express = x.substring(5);
-					}
-					if(x.contains("assume")) {
-						reason = "assume";
-						express = x.substring(7);
-					}
-					if(x.contains("mp")) {
-						reason = "mp";
-						express = x.substring(3);
-					}
-					if(x.contains("mt")) {
-						reason = "mt";
-						express = x.substring(3);
-					}
-					if(x.contains("co")) {
-						reason = "co";
-						express = x.substring(3);
-					}
-					if(x.contains("ic")) {
-						reason = "ic";
-						express = x.substring(3);
-					}
-					if(x.contains("repeat")) {
-						reason = "repeat";
-						express = x.substring(7);
-					}
-					//handle theorems
-				}
+	private static class ExpNode {
+		
+		public Object myItem;
+		public ExpNode myLeft;
+		public ExpNode myRight;
+		
+		public ExpNode (Object obj) {
+			myItem = obj;
+			myLeft = myRight = null;
+		}
+		
+		public ExpNode (Object obj, ExpNode left, ExpNode right) {
+			myItem = obj;
+			myLeft = left;
+			myRight = right;
+		}
+		
+		public boolean isLeaf() {
+			if (myLeft == null && myRight == null) {
+				return true;
+			} else {
+				return false;
 			}
+		}
+		
+		// not sure if this will be necessary
+		public boolean isMyItemInt() {
+			if (myItem instanceof Integer) return true;
+			try {
+				int x = Integer.parseInt((String) myItem);
+				return true;
+			} catch (NumberFormatException e) {
+				return false;
+			}
+		}
+		
+	}
 }
--- Proof.java
+++ Proof.java
@@ -1,18 +1,59 @@
 import java.util.*;
 
 public class Proof {
+	private LineNumber ln;
+	private String reason;
+	private Expression exp;
 
 	public Proof (TheoremSet theorems) {
+		ln = new LineNumber();
 	}
 
 	public LineNumber nextLineNumber ( ) {
-		return null;
+		return ln;
 	}
 
 	public void extendProof (String x) throws IllegalLineException, IllegalInferenceException {
-			Expression exp = new Expression(x);
+		System.out.println("Entered: " + x);
+		x = x.trim();
+		// is print?
+		if (x.equals("print")) {
+			// TODO: print proof so far
+		} else {
+			// try to split into reason and expression
+			if(x.startsWith("show")){
+				reason = "show";
+				exp = new Expression(x.substring(5));
+			}
+			if(x.startsWith("assume")) {
+				reason = "assume";
+				exp = new Expression(x.substring(7));
+			}
+			if(x.startsWith("mp")) {
+				reason = "mp";
+				exp = new Expression(x.substring(3));
+			}
+			if(x.startsWith("mt")) {
+				reason = "mt";
+				exp = new Expression(x.substring(3));
+			}
+			if(x.startsWith("co")) {
+				reason = "co";
+				exp = new Expression(x.substring(3));
+			}
+			if(x.contains("ic")) {
+				reason = "ic";
+				exp = new Expression(x.substring(3));
+			}
+			if(x.contains("repeat")) {
+				reason = "repeat";
+				exp = new Expression(x.substring(7));
+			} 
+			System.out.println("reason: " + reason + "\nexpression: ");
+			exp.print();
+		}
 	}
-
+	
 	public String toString ( ) {
 		return "";
 	}
