37f8a195b2ebba40f362cbb8f2662fb88d172f51
chashemi <camhashemi@gmail.com>
2013-07-24 12:29:57 -0700

    Update LineNumber.java
    Created a List<String> lines to store each line (for Proof.print()). Everytime ln.add() or ln.addPoint() is called, lines.add(toString()) adds the new LineNumber string to the ArrayList of Lines.
    
    Wrote isLegal to check for any non integer or non ".".
    
    Overloaded isLegal to take in a string and the current LineNumber. Created a function lineLevel to count the number of "." in a LineNumber. If the input string to isLegal has more "." than the current LineNumber, return false.

--- LineNumber.java
+++ LineNumber.java
@@ -5,21 +5,27 @@ import com.sun.org.apache.bcel.internal.generic.LASTORE;
 
 public class LineNumber {
 	private List<Integer> ln;
+	List<String> lines;
 	
 	public LineNumber() {
 		ln = new ArrayList<Integer>();
+		lines = new ArrayList<String>();
 		ln.add(1);
+		lines.add(toString());
 	}
 	
 	public LineNumber(String str) {
+		//assert isLegal(str, ln);
 		String[] strpts = str.split(".");
 		for (String part : strpts) {
 			ln.add(Integer.parseInt(part));
 		}
+		lines.add(toString());
 	}
 	
 	public void addPoint() {
 		ln.add(1);
+		lines.add(toString());
 	}
 	
 	// in the course of extending a proof, if an inference is valid
@@ -28,6 +34,7 @@ public class LineNumber {
 		ln.remove(ln.size()-1);
 		int last = ln.get(ln.size()-1);
 		ln.set(ln.size()-1, last+1);
+		lines.add(toString());
 	}
 	
 	public void increment() {
@@ -37,11 +44,43 @@ public class LineNumber {
 			int last = ln.get(ln.size()-1);
 			ln.set(ln.size()-1, last+1);
 		}
+		lines.add(toString());
+;	}
+	
+	public static boolean isLegal(String str) throws IllegalLineException {
+		int i = 0; String s;
+		while (i < str.length()){
+			s = str.substring(i,i+1);
+			try {
+				Integer.parseInt(s);
+				i++;
+			} catch (NumberFormatException e){
+				if (s.equals(".")){
+					i++;
+					continue;
+				} else {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 	
-	public static boolean isLegal(String str) {
-	// TODO 
+	public static boolean isLegal(String str, LineNumber ln){
+		if (lineLevel(str) >= lineLevel(ln.toString())){
+			return false;
+		}
 		return true;
+	}
+	
+	private static int lineLevel (String str){
+		int count = 0;
+		for (int i = 0; i < str.length(); i++){
+			if (str.charAt(i) == '.'){
+				count++;
+			}
+		}
+		return count;
 	}
 	
 	public String toString() {
