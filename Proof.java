f8c9526e2a0c69d53caaed11831256e68bc6182c
chashemi <camhashemi@gmail.com>
2013-07-24 12:14:32 -0700

    Update Proof.java
    Implemented printQueue as a LinkedList (which implements the Queue interface), and thus the "print" function. X (input) is added to printQueue after every run of extendProof().
    
    Changed HashMap parameters to <String,Expression>, so that it may work for names in TheoremSet. This required changing all of the arguments to facts.put(), so take in ln.toString() instead of ln.
    
    Implemented toString(), which isn't necessary with our current implementation of "print", but we could use it if we wanted to.

--- Proof.java
+++ Proof.java
@@ -3,28 +3,31 @@ import java.util.*;
 public class Proof {
 	private LineNumber ln;
 	private Stack<Expression> showing;
-	//private Queue<String> printQueue;
-	private HashMap<LineNumber,Expression> facts; // not great, should be able to have name as key, for theorems
-
+	private HashMap<String,Expression> facts; // changed key to String (allows for names) LineNumber.toString() should be called when adding to HashMaps
+	private LinkedList<String> printQueue;
+	
 	public Proof (TheoremSet theorems) {
 		ln = new LineNumber();
 		showing = new Stack<Expression>();
-		//printQueue = new Queue<String>();
-		facts = new HashMap<LineNumber,Expression>();
-		
+		facts = new HashMap<String,Expression>();
+		printQueue = new LinkedList<String>();
 	}
-
-	public LineNumber nextLineNumber ( ) {
+	
+	public LineNumber nextLineNumber(){
 		return ln;
 	}
 
 	public void extendProof (String x) throws IllegalLineException, IllegalInferenceException {
 		// System.out.println("Entered: " + x);
-		printAllFacts();
+		//printAllFacts();
 		x = x.trim();
 		// is print?
 		if (x.equals("print")) {
-			// TODO: print proof so far
+			for (int i = 0; i < printQueue.size(); i++){
+				System.out.print(ln.lines.get(i) + "\t");
+				System.out.println(printQueue.get(i));
+			}
+			ln.increment();
 		} else {
 			// try to split into reason and expression
 			String[] parts = x.split(" ");
@@ -40,18 +43,17 @@ public class Proof {
 				} else {
 					ln.addPoint();
 				}
-				
 			// "assume" exp
 			} else if (parts[0].equals("assume") 
 					&& Expression.isLegal(parts[1]) && parts.length == 2) {
 				//		add exp to "facts" array (facts are assumptions, inferred results, theorems)
 				//		increment line number	
-				facts.put(ln, new Expression(parts[1]));
+				facts.put(ln.toString(), new Expression(parts[1]));
 				ln.increment();
 
 			// "mp" ln1 ln2 exp
 			} else if (parts[0].equals("mp")
-					&& LineNumber.isLegal(parts[1]) && LineNumber.isLegal(parts[2])
+					&& LineNumber.isLegal(parts[1], ln) && LineNumber.isLegal(parts[2], ln)
 					&& Expression.isLegal(parts[3]) && parts.length == 4) {
 			//		check that ln1 is the left branch of ln2
 			//		check that exp is the right branch of ln2
@@ -69,7 +71,7 @@ public class Proof {
 				Expression e1then2 = getFactByLineNumber(new LineNumber(parts[2]));
 				Expression e2 = new Expression(parts[3]);
 				if (e1.isLeftBranchOf(e1then2) && e2.isRightBranchOf(e1then2)) {
-					facts.put(ln, e2);
+					facts.put(ln.toString(), e2);
 					if (e2.equals(showing.peek())) showing.pop();
 					ln.resetPoint();
 				} else {
@@ -78,22 +80,22 @@ public class Proof {
 				
 			// "mt" ln1 ln2 exp
 			} else if (parts[0].equals("mt")
-					&& LineNumber.isLegal(parts[1]) && LineNumber.isLegal(parts[2])
+					&& LineNumber.isLegal(parts[1], ln) && LineNumber.isLegal(parts[2], ln)
 					&& Expression.isLegal(parts[3]) && parts.length == 4) {
 				
 				// TODO
 				
 			// "co" ln1 ln2 exp
 			} else if (parts[0].equals("co")
-					&& LineNumber.isLegal(parts[1]) && LineNumber.isLegal(parts[2])
+					&& LineNumber.isLegal(parts[1], ln) && LineNumber.isLegal(parts[2], ln)
 					&& Expression.isLegal(parts[3]) && parts.length == 4) {
 					
 				// TODO
 				
 			// "ic" ln1 exp
-			} else if (parts[0].equals("ic") 
-					&& LineNumber.isLegal(parts[1]) 
-					&& Expression.isLegal(parts[2]) && parts.length == 3) {
+			} else if (parts[0].equals("ic") && parts.length == 3 
+					&& LineNumber.isLegal(parts[1], ln) 
+					&& Expression.isLegal(parts[2])) {
 				// 		check that ln1 is the right branch of exp
 				//		if true, 
 				//			add exp to "facts"
@@ -105,7 +107,7 @@ public class Proof {
 				LineNumber refln = new LineNumber(parts[1]);
 				Expression e = new Expression(parts[2]);
 				if (getFactByLineNumber(refln).isRightBranchOf(e)) {
-					facts.put(ln, e);
+					facts.put(ln.toString(), e);
 					if (e.equals(showing.peek())) showing.pop();
 					ln.resetPoint();
 				} else {
@@ -115,7 +117,7 @@ public class Proof {
 
 			// "repeat" ln1 exp
 			} else if (parts[0].equals("repeat") 
-					&& LineNumber.isLegal(parts[1]) 
+					&& LineNumber.isLegal(parts[1], ln) 
 					&& Expression.isLegal(parts[2]) && parts.length == 3) {
 				
 				// TODO
@@ -126,6 +128,8 @@ public class Proof {
 				throw new IllegalLineException("Wrong number of things");
 			}
 			
+			printQueue.add(x);
+			
 			// Now that we've done input exception checking, 
 			// it's safe to add the line to print queue 
 			// printQueue.add(x);
@@ -135,7 +139,11 @@ public class Proof {
 	}
 	
 	public String toString ( ) {
-		return "";
+		String str = "";
+		for (int i = 0; i < printQueue.size(); i++){
+			str += printQueue.get(i) + "\n";
+		}
+		return str;
 	}
 
 	public boolean isComplete ( ) {
@@ -144,15 +152,15 @@ public class Proof {
 	}
 	
 	private Expression getFactByLineNumber(LineNumber ln) {
-		return facts.get(ln);
+		return facts.get(ln.toString());
 	}
 	
 	// for debugging, remove later
 	private void printAllFacts() {
-		Iterator<LineNumber> it = facts.keySet().iterator();
+		Iterator<String> it = facts.keySet().iterator();
 		while (it.hasNext()) {
-			LineNumber key = it.next();
-			System.out.println(key.toString() + " - " + facts.get(key).toInorderString());
+			String key = it.next();
+			System.out.println(key + " - " + facts.get(key).toInorderString());
 		}
 	}
 }
