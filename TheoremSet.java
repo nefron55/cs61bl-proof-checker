d120acf651703066715bea2f06b60613f65af83f
chashemi <camhashemi@gmail.com>
2013-07-27 17:16:59 -0700

    Update TheoremSet.java
    Basic file, haven't implemented integration with ProofChecker/InputSource (TODO tonight).
    
    Accumulates facts from input file until file is exhausted, then Proof.facts = TheoremSet.facts.

--- TheoremSet.java
+++ TheoremSet.java
@@ -1,9 +1,22 @@
+import java.util.*;
+
 public class TheoremSet {
+	
+	private HashMap<String,Expression> facts;
 
 	public TheoremSet ( ) {
+		facts = new HashMap<String,Expression>();
+	}
+	
+	public boolean contains(String s){
+		return facts.containsKey(s);
 	}
 
-	public Expression put (String s, Expression e) {
-		return null;
+	public void put (String s, Expression e) {
+		facts.put(s, e);
+	}
+	
+	public HashMap<String,Expression> transfer(){
+		return facts;
 	}
 }
