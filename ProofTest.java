f08d7804499dc7e5c7b548a392abb3d32c32ffda
chashemi <camhashemi@gmail.com>
2013-07-28 19:16:02 -0700

    Update ProofTest.java

--- ProofTest.java
+++ ProofTest.java
@@ -100,151 +100,162 @@ public class ProofTest extends TestCase {
 			}
 		}
 		
-		public void testFacts() {
-			
+		public void testisNegation(){
+			try {
+				Expression e = new Expression("~p");
+				Expression e1 = new Expression("~(p|q)");
+				Expression e2 = new Expression("a");
+				Expression e3 = new Expression("(p=>q)");
+				Expression e4 = new Expression("~(p=>q)");
+				assertTrue(e.isNegation());
+				assertTrue(e1.isNegation());
+				assertFalse(e2.isNegation());
+				assertFalse(e3.isNegation());
+				assertTrue(e4.isNegation());
+			} catch (IllegalLineException e){
+				fail(e.getMessage());
+			}
 		}
 		
-		public void testLineNumber() {
-			
-		}
-		
-		public void testisNegation() throws IllegalLineException {
-			Expression e = new Expression("~p");
-			Expression e1 = new Expression("~(p|q)");
-			Expression e2 = new Expression("a");
-			Expression e3 = new Expression("(p=>q)");
-			Expression e4 = new Expression("~(p=>q)");
-			assertTrue(e.isNegation());
-			assertTrue(e1.isNegation());
-			assertFalse(e2.isNegation());
-			assertFalse(e3.isNegation());
-			assertTrue(e4.isNegation());
-		}
-		
-		public void testisFollows() throws IllegalLineException {
-			Expression e = new Expression("~(p=>q)");
-			Expression e1 = new Expression("~(p|q)");
-			Expression e2 = new Expression("a");
-			Expression e3 = new Expression("(p=>q)");
-			assertFalse(e.isFollows());
-			assertFalse(e1.isFollows());
-			assertFalse(e2.isFollows());
-			assertTrue(e3.isFollows());
-		}
-	
-
-		public static void testProofs() throws IllegalLineException, IllegalInferenceException {
-			/*TheoremSet t = new TheoremSet();
-			Proof p = new Proof(t);
-			p.extendProof("show (p=>(~p=>q))");*/
+		public void testisFollows(){
+			try {
+				Expression e = new Expression("~(p=>q)");
+				Expression e1 = new Expression("~(p|q)");
+				Expression e2 = new Expression("a");
+				Expression e3 = new Expression("(p=>q)");
+				assertFalse(e.isFollows());
+				assertFalse(e1.isFollows());
+				assertFalse(e2.isFollows());
+				assertTrue(e3.isFollows());
+			} catch (IllegalLineException e){
+				fail(e.getMessage());
+			}
 		}
 
-		public static void testPrint() throws IllegalLineException, IllegalInferenceException {
-			TheoremSet t = new TheoremSet();
-			Proof p = new Proof(t);
-			p.startDebugging();
-			p.extendProof("show (p=>q)");
-			p.extendProof("assume a");
-			p.extendProof("assume b");
-			p.extendProof("assume c");
-			p.extendProof("show p");
-			p.extendProof("assume p");
-			p.extendProof("assume q");
-			p.extendProof("print");
+		public static void testPrint() {
+			try {
+				TheoremSet t = new TheoremSet();
+				Proof p = new Proof(t);
+				p.startDebugging();
+				p.extendProof("show (p=>q)");
+				p.extendProof("assume a");
+				p.extendProof("assume b");
+				p.extendProof("assume c");
+				p.extendProof("show p");
+				p.extendProof("assume p");
+				p.extendProof("assume q");
+				p.extendProof("print");
+			} catch (IllegalLineException e){
+				fail(e.getMessage());
+			} catch (IllegalInferenceException f){
+				fail(f.getMessage());
+			}
 		}
 		
-		public static void testMP() throws IllegalLineException, IllegalInferenceException {
-			TheoremSet t = new TheoremSet();
-			Proof p = new Proof(t);
-			p.startDebugging();
-			p.extendProof("show q");
-			p.extendProof("assume p");
-			p.extendProof("assume (p=>q)");
-			p.extendProof("mp 2 3 q");
-			
-			Proof p2 = new Proof(t);
-			p2.startDebugging();
-			p2.extendProof("show (q=>p)");
-			p2.extendProof("assume (p=>q)");
-			p2.extendProof("assume ((p=>q)=>(q=>p))");
-			p2.extendProof("mp 2 3 (q=>p)");
-			
-			Proof p3 = new Proof(t);
-			p3.startDebugging();
-			p3.extendProof("show ~(q=>p)");
-			p3.extendProof("assume ~(p=>q)");
-			p3.extendProof("assume (~(p=>q)=>~(q=>p))");
-			p3.extendProof("mp 2 3 ~(q=>p)");
-			
-			Proof p4 = new Proof(t);
-			p4.startDebugging();
-			p4.extendProof("show (~(p=>q)=>~(k=>l))");
-			p4.extendProof("assume ((p=>q)=>(k=>l))");
-			p4.extendProof("assume (((p=>q)=>(k=>l))=>(~(p=>q)=>~(k=>l)))");
-			p4.extendProof("mp 2 3 (~(p=>q)=>~(k=>l))");
+		public static void testMP(){
+			try {
+				TheoremSet t = new TheoremSet();
+				Proof p = new Proof(t);
+				p.startDebugging();
+				p.extendProof("show q");
+				p.extendProof("assume p");
+				p.extendProof("assume (p=>q)");
+				p.extendProof("mp 2 3 q");
+				
+				Proof p2 = new Proof(t);
+				p2.startDebugging();
+				p2.extendProof("show (q=>p)");
+				p2.extendProof("assume (p=>q)");
+				p2.extendProof("assume ((p=>q)=>(q=>p))");
+				p2.extendProof("mp 2 3 (q=>p)");
+				
+				Proof p3 = new Proof(t);
+				p3.startDebugging();
+				p3.extendProof("show ~(q=>p)");
+				p3.extendProof("assume ~(p=>q)");
+				p3.extendProof("assume (~(p=>q)=>~(q=>p))");
+				p3.extendProof("mp 2 3 ~(q=>p)");
+				
+				Proof p4 = new Proof(t);
+				p4.startDebugging();
+				p4.extendProof("show (~(p=>q)=>~(k=>l))");
+				p4.extendProof("assume ((p=>q)=>(k=>l))");
+				p4.extendProof("assume (((p=>q)=>(k=>l))=>(~(p=>q)=>~(k=>l)))");
+				p4.extendProof("mp 2 3 (~(p=>q)=>~(k=>l))");
+			} catch(IllegalLineException e){
+				fail(e.getMessage());
+			} catch(IllegalInferenceException e){
+				fail(e.getMessage());
+			}
 		}
 		
-		public static void testMT() throws IllegalLineException, IllegalInferenceException {
-			TheoremSet t = new TheoremSet();
-			Proof p = new Proof(t);
-			p.startDebugging();
-			p.extendProof("show ~p");
-			p.extendProof("assume ~q");
-			p.extendProof("assume (p=>q)");
-			p.extendProof("mt 2 3 ~p");
-			
-			Proof p2 = new Proof(t);
-			p2.startDebugging();
-			p2.extendProof("show ~(p=>q)");
-			p2.extendProof("assume ~(q=>p)");
-			p2.extendProof("assume ((p=>q)=>(q=>p))");
-			p2.extendProof("mt 2 3 ~(p=>q)");
-			
-			Proof p4 = new Proof(t);
-			p4.startDebugging();
-			p4.extendProof("show ~((p=>q)=>(q=>r))");
-			p4.extendProof("assume ~((a=>b)=>(b=>c))");
-			p4.extendProof("assume (((p=>q)=>(q=>r))=>((a=>b)=>(b=>c)))");
-			p4.extendProof("mt 2 3 ~((p=>q)=>(q=>r))");
+		public static void testMT(){
+			try {
+				TheoremSet t = new TheoremSet();
+				Proof p = new Proof(t);
+				p.startDebugging();
+				p.extendProof("show ~p");
+				p.extendProof("assume ~q");
+				p.extendProof("assume (p=>q)");
+				p.extendProof("mt 2 3 ~p");
+				
+				Proof p2 = new Proof(t);
+				p2.startDebugging();
+				p2.extendProof("show ~(p=>q)");
+				p2.extendProof("assume ~(q=>p)");
+				p2.extendProof("assume ((p=>q)=>(q=>p))");
+				p2.extendProof("mt 2 3 ~(p=>q)");
+				
+				Proof p4 = new Proof(t);
+				p4.startDebugging();
+				p4.extendProof("show ~((p=>q)=>(q=>r))");
+				p4.extendProof("assume ~((a=>b)=>(b=>c))");
+				p4.extendProof("assume (((p=>q)=>(q=>r))=>((a=>b)=>(b=>c)))");
+				p4.extendProof("mt 2 3 ~((p=>q)=>(q=>r))");
+			} catch (IllegalLineException e){
+				fail(e.getMessage());
+			} catch (IllegalInferenceException e){
+				fail(e.getMessage());
+			}
 		}
 		
-		public static void testIC() throws IllegalLineException, IllegalInferenceException {
-			//System.out.println("-------------------------------");
-			
-			TheoremSet t = new TheoremSet();
-			Proof p = new Proof(t);
-			p.startDebugging();
-			p.extendProof("show (p=>q)");
-			p.extendProof("assume q");
-			p.extendProof("ic 2 (p=>q)");
-			
-			p = new Proof(t);
-			p.startDebugging();
-			p.extendProof("show (~(p=>q)=>(p=>q))");
-			p.extendProof("assume (p=>q)");
-			p.extendProof("ic 2 (~(p=>q)=>(p=>q))");
+		public static void testIC(){
+			try {
+				TheoremSet t = new TheoremSet();
+				Proof p = new Proof(t);
+				p.startDebugging();
+				p.extendProof("show (p=>q)");
+				p.extendProof("assume q");
+				p.extendProof("ic 2 (p=>q)");
+				
+				p = new Proof(t);
+				p.startDebugging();
+				p.extendProof("show (~(p=>q)=>(p=>q))");
+				p.extendProof("assume (p=>q)");
+				p.extendProof("ic 2 (~(p=>q)=>(p=>q))");
+			} catch (IllegalLineException e){
+				fail(e.getMessage());
+			} catch (IllegalInferenceException e){
+				fail(e.getMessage());
+			}
 						
 		}
 		
-		public static void testRepeat() throws IllegalLineException, IllegalInferenceException {
-			//System.out.println("-------------------------------");
-			TheoremSet t = new TheoremSet();
-			Proof p = new Proof(t);
-			p.extendProof("show (p=>p)");
-			p.extendProof("show (p=>p)");
-			p.extendProof("assume p");
-			p.extendProof("ic 2.1 (p=>p)");
-			p.extendProof("print");
-			p.extendProof("repeat 2 (p=>p)");
+		public static void testRepeat(){
+			try {
+				TheoremSet t = new TheoremSet();
+				Proof p = new Proof(t);
+				p.extendProof("show (p=>p)");
+				p.extendProof("show (p=>p)");
+				p.extendProof("assume p");
+				p.extendProof("ic 2.1 (p=>p)");
+				p.extendProof("print");
+				p.extendProof("repeat 2 (p=>p)");
+			} catch (IllegalLineException e){
+				fail(e.getMessage());
+			} catch (IllegalInferenceException e){
+				fail(e.getMessage());
+			}
 		}
-
-		/*public static void testnotMP() throws IllegalLineException, IllegalInferenceException {	
-			TheoremSet t = new TheoremSet();
-			Proof p = new Proof(t);
-			p.extendProof("show q");
-			p.extendProof("assume ~p");
-			p.extendProof("assume (~p=>q)");
-			p.extendProof("mp 2 3 q");
-		}*/
 		
 	}
