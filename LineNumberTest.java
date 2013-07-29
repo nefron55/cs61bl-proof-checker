04e0579afa547466873a59d7311173432ae04ce3
Peter Polinsky <ppolinsky@gmail.com>
2013-07-28 18:43:28 -0700

    don't throw exceptions in LineNumberTest

--- LineNumberTest.java
+++ LineNumberTest.java
@@ -3,7 +3,7 @@ import junit.framework.TestCase;
 
 public class LineNumberTest extends TestCase {
 	
-	public void testaddPoint() throws IllegalLineException {
+	public void testaddPoint() {
 		LineNumber n = new LineNumber();
 		n.addPoint();
 		assertEquals(n.toString(), "1.1");
@@ -11,13 +11,17 @@ public class LineNumberTest extends TestCase {
 		assertEquals(n.toString(), "1.1.1");
 		n.increment();
 		assertEquals(n.toString(), "1.1.2");
-		n.removePeriod();
+		try {
+			n.removePeriod();
+		} catch (IllegalLineException e) {
+			fail(e.getMessage());
+		}
 		assertEquals(n.toString(), "1.1");
 		n.addPoint();
 		assertEquals(n.toString(), "1.1.1");
 	}
 	
-	public void testRemovePeriod() throws IllegalLineException{
+	public void testRemovePeriod() {
 		LineNumber n = new LineNumber();
 		n.increment();
 		assertEquals(n.toString(), "2");
@@ -27,20 +31,24 @@ public class LineNumberTest extends TestCase {
 		assertEquals(n.toString(), "3.1");
 		n.addPoint();
 		assertEquals(n.toString(), "3.1.1");
-		n.removePeriod();
-		assertEquals(n.toString(), "3.1");
-		n.removePeriod();
-		assertEquals(n.toString(), "3");
 		try {
 			n.removePeriod();
-			fail("Missing exception");
-		} catch(IllegalLineException e) {
-			assertEquals("No period to remove", e.getMessage());
+			assertEquals(n.toString(), "3.1");
+			n.removePeriod();
+			assertEquals(n.toString(), "3");
+			try {
+				n.removePeriod();
+				fail("Missing exception");
+			} catch(IllegalLineException e) {
+				assertEquals("No period to remove", e.getMessage());
+			}
+		} catch (IllegalLineException e) {
+			fail(e.getMessage());
 		}
 	}
 	
 
-	public void testIncrement() throws IllegalLineException{
+	public void testIncrement() {
 		LineNumber n = new LineNumber();
 		n.increment();
 		assertEquals(n.toString(), "2");
@@ -59,7 +67,7 @@ public class LineNumberTest extends TestCase {
 		assertEquals(n.toString(), "2.3");
 	}
 	
-	public void testisLegal() throws IllegalLineException {
+	public void testisLegal() {
 		try{
 			LineNumber n = new LineNumber("a.b.d.c.s");
 			fail("Missing exception");
@@ -68,23 +76,31 @@ public class LineNumberTest extends TestCase {
 		}	
 	}
 	
-	public void testisLegalReference() throws IllegalLineException{
+	public void testisLegalReference() {
 		LineNumber n = new LineNumber();
 		LineNumber m = new LineNumber();
 		n.increment();
 		n.addPoint();
 		m.increment();
 		m.increment();
-		assertFalse(n.isLegalReference(m.toString()));
+		try {
+			assertFalse(n.isLegalReference(m.toString()));
+		} catch (IllegalLineException e) {
+			fail(e.getMessage());
+		}
 		LineNumber p = new LineNumber();
 		LineNumber o = new LineNumber();
 		p.increment();
 		o.increment();
 		o.addPoint();
-		assertTrue(o.isLegalReference(p.toString()));
+		try {
+			assertTrue(o.isLegalReference(p.toString()));
+		} catch (IllegalLineException e) {
+			fail(e.getMessage());
+		}
 	}
 	
-	public void testLineLevel() throws IllegalLineException {
+	public void testLineLevel() {
 		LineNumber n = new LineNumber();
 		n.increment();
 		assertEquals(n.toString(), "2");
